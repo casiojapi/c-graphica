c706e0c212ba4b756d3c6d060620ccaf87e6b11e
viieja <msantoro@fi.uba.ar>
2020-10-07 17:17:42 -0300

    fixed bug/typo in netpbm main

--- netpbm/main.c
+++ netpbm/main.c
@@ -12,7 +12,7 @@
 #define Y0 (ALTO / 2)
 
 #define FRECUENCIA 0.0000733
-
+        
 // ej-1 defines
 
 int main(int argc, char const *argv[]){
@@ -29,7 +29,7 @@ int main(int argc, char const *argv[]){
     fprintf(image, "%d %d\n %d\n", ANCHO, ALTO, MAXIMO);
     for (size_t i = 0; i < ALTO; i++){
         for (int j = 0; j < ANCHO; j++){
-            double val = scaled_sin(distance(j, i ,X0, Y0), i * FRECUENCIA, MAXIMO);
+            double val = scaled_sin(distance(j, i , X0, Y0), i * FRECUENCIA, MAXIMO);
             fprintf(image, "%d\t %d %d\n", (int)(val*0.6333221), (int)(val * .8 * j), (int)(val*.91132));
         }
     }
